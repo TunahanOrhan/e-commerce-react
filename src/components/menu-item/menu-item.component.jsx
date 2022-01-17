3bd0a811487d07e483d33c29ba957d7808997b29
Tunahan Orhan <61620057+TunahanOrhan@users.noreply.github.com>
2022-01-17 17:31:23 +0300

    added menu item component

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "./menu-item.styles.scss";
 
-const MenuItem = ({ title, imageUrl, size }) => (
+const MenuItem = ({ title, imageUrl, size, linkUrl }) => (
   <div className={`${size} menu-item`}>
     <div
       className="background-image"
@@ -10,7 +11,9 @@ const MenuItem = ({ title, imageUrl, size }) => (
     />
     <div className="content">
       <h1 className="title">{title.toUpperCase()}</h1>
-      <span className="subtitle">SHOP NOW</span>
+      <Link to={linkUrl} className="subtitle">
+        <span>SHOP NOW</span>
+      </Link>
     </div>
   </div>
 );
